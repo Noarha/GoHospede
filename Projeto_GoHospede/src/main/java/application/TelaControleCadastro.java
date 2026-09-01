ec27394ea60ee589a06d8b80070597283604c97b
aluno <aluno@REF47336LAB1PC.edu.ce.senac.br>
2026-09-01 20:52:29 -0300

    Alterei os ícones

--- Projeto_GoHospede/src/main/java/application/TelaControleCadastro.java
+++ Projeto_GoHospede/src/main/java/application/TelaControleCadastro.java
@@ -1,11 +1,30 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class TelaControleCadastro {
 
     @FXML
     private AnchorPane Telalogin;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtconfirmarsenha;
+
+    @FXML
+    private TextField txtnascimento;
+
+    @FXML
+    private TextField txtnome;
+
+    @FXML
+    private TextField txtsenha;
+
+    @FXML
+    private TextField txttelefone;
 
 }
