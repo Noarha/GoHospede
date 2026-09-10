f2a802d25b35cec9c92d807c2b5e8dc329c07560
aluno <aluno@REF47336LAB1PC.edu.ce.senac.br>
2026-09-10 19:42:20 -0300

    Projeto juntado

---
+++ Projeto_GoHospede/src/main/java/application/TelaControleLogin.java
@@ -0,0 +1,50 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class TelaControleLogin {
+
+    @FXML
+    private Button Entrarbotton;
+
+    @FXML
+    private ImageView Tela;
+
+    @FXML
+    private Text acesse;
+
+    @FXML
+    private Text bemvindo;
+
+    @FXML
+    private Pane cadastro;
+
+    @FXML
+    private Hyperlink cadastros;
+
+    @FXML
+    private Text email;
+
+    @FXML
+    private TextField emailescrever;
+
+    @FXML
+    private Text login;
+
+    @FXML
+    private Text senha;
+
+    @FXML
+    private PasswordField senhaescrever;
+
+    @FXML
+    private TextField senhaescrever2;
+
+}
