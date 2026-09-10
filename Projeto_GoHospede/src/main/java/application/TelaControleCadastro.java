72622acf034870813e91701adfb975315a982ce8
aluno <aluno@REF47336LAB1PC.edu.ce.senac.br>
2026-09-10 19:23:53 -0300

    Commit atualizado

--- Projeto_GoHospede/src/main/java/application/TelaControleCadastro.java
+++ Projeto_GoHospede/src/main/java/application/TelaControleCadastro.java
@@ -1,13 +1,94 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class TelaControleCadastro {
 
     @FXML
+    private CheckBox CheckBox;
+
+    @FXML
+    private Hyperlink Entrar;
+
+    @FXML
+    private Hyperlink PoliticaPri;
+
+    @FXML
     private AnchorPane Telalogin;
+
+    @FXML
+    private Hyperlink TermoDUso;
+
+    @FXML
+    private Button buttonCadastro;
+
+    @FXML
+    private ImageView imagem;
+
+    @FXML
+    private ImageView imgCadeado;
+
+    @FXML
+    private ImageView imgCadeado2;
+
+    @FXML
+    private ImageView imgData;
+
+    @FXML
+    private ImageView imgEmail;
+
+    @FXML
+    private ImageView imgPerfil;
+
+    @FXML
+    private ImageView imgSeta;
+
+    @FXML
+    private ImageView imgTelefone;
+
+    @FXML
+    private Pane pNome;
+
+    @FXML
+    private Pane pTelefone;
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private Pane panesenha;
+
+    @FXML
+    private Pane pconfisenha;
+
+    @FXML
+    private Pane pdataNascimento;
+
+    @FXML
+    private Pane pemail;
+
+    @FXML
+    private Text text;
+
+    @FXML
+    private Text texto;
+
+    @FXML
+    private Text texto2;
+
+    @FXML
+    private Text texto3;
+
+    @FXML
+    private Text texto4;
 
     @FXML
     private TextField txtEmail;
